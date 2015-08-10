026e7f440a02e5c769346808d59ea378f60d7bb8
samdbeckham <sam@beckham.io>
2015-08-10 17:56:57 +0100

    prepping gulp for the build tasks

--- gulp/tasks.js
+++ gulp/tasks.js
@@ -2,6 +2,10 @@ var gulp = require('gulp'),
     runSequence = require('run-sequence');
 
 gulp.task('default', function(cb) {
+    runSequence('dev');
+});
+
+gulp.task('dev', function(cb) {
     runSequence([
         'sass:dev',
         'js:dev',
