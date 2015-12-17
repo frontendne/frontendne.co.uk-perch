5e8632afc31fdf9fdd505393d83d63a974215314
Martin Underhill <martin@tempertemper.net>
2015-12-17 18:12:21 +0000

    tidies main sass file

--- gulp/scss-lint.js
+++ gulp/scss-lint.js
@@ -5,6 +5,7 @@ var gulp = require('gulp'),
 gulp.task('scss-lint', function () {
     gulp.src([
             './assets/scss/**/*.scss',
+            '!./assets/scss/screen.scss',
             '!./assets/scss/base/_shame.scss',
             '!./assets/scss/base/_reset.scss',
         ]).pipe(scsslint({
