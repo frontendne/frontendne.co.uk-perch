e7fc4372263bb3fabf819a25d89b779d3bc66ca9
Martin Underhill <martin@tempertemper.net>
2016-02-01 15:39:20 +0000

    sponsorts page template

---
+++ web/cms/templates/pages/sponsors.php
@@ -0,0 +1,11 @@
+<?php
+
+  // Header
+  perch_layout('global/head');
+
+  // Page content
+  perch_content('Primary content');
+  perch_content('Contact form');
+
+  // Footer
+  perch_layout('global/footer_no_content');
