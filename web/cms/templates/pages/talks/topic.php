cccf2c740eb4f7f4d657449ceba2085c5182dc99
Martin Underhill <martin@tempertemper.net>
2016-02-29 09:26:54 +0000

    adds talk by topic filter

---
+++ web/cms/templates/pages/talks/topic.php
@@ -0,0 +1,32 @@
+<?php
+
+  // Header
+  perch_layout('global/head', [
+    'body-class' => 'talks',
+  ]);
+
+  // Page content
+
+  echo '<div>';
+
+  if (perch_get('cat')) {
+    perch_category('topic/'.perch_get('cat').'/',array(
+      'template' => 'topic_header.html'
+    ));
+  } else {
+    echo('Talks');
+  }
+
+  if (perch_get('cat')) {
+    perch_collection('Talks', [
+      'category' => 'topic/'.perch_get('cat'),
+      'template' => 'topic_listing.html',
+    ]);
+  } else {
+    perch_categories(array(
+      'set'=>'topic',
+    ));
+  }
+
+  // Footer
+  perch_layout('global/footer');
