b438c5ebc8ee7e9a98fe11487013308936b456dd
Martin Underhill <martin@tempertemper.net>
2016-03-01 14:48:07 +0000

    adds sponsors specific form

--- web/cms/templates/pages/sponsors.php
+++ web/cms/templates/pages/sponsors.php
@@ -5,7 +5,7 @@
 
   // Page content
   perch_content('Primary content');
-  perch_content('Contact form');
+  perch_content('Sponsor contact form');
 
   // Footer
   perch_layout('global/footer_no_content');
