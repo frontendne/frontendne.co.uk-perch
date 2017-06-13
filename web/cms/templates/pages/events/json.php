570c6938f54e762566562aa7b8b0d62dca759e74
Martin Underhill <martin@tempertemper.net>
2017-06-13 10:51:18 +0100

    tidies up json file and fixes favicon references

--- web/cms/templates/pages/events/json.php
+++ web/cms/templates/pages/events/json.php
@@ -8,29 +8,29 @@
         'title'         => 'Frontend NE events',
         'home_page_url' => PerchUtil::html($domain),
         'feed_url'      => PerchUtil::html($domain).'/events/feed.json',
-        'icon'          => PerchUtil::html($domain).'/assets/img/feedburd.png',
-        'favicon'       => PerchUtil::html($domain).'/assets/img/burd1.png',
+        'icon'          => PerchUtil::html($domain).'/cms/addons/feathers/frontendne/img/icons/apple-touch-icon-precomposed-180x180.png',
+        'favicon'       => PerchUtil::html($domain).'/cms/addons/feathers/frontendne/img/icons/favicon.png',
         'items'         => [],
     ];
 
     $items = perch_collection('Events', [
-        'sort'          => 'date',
+        'sort'          => 'date',a
         'sort-order'    => 'DESC',
         'skip-template' => true,
         'count'         => 10,
    ]);
 
     if (count($items)) {
-    foreach($items as $item) {
-        $feed['items'][] = [
-            'id'             => $item['_id'],
-            'date_published' => date('c', strtotime($item['date'])),
-            'title'          => $item['title'],
-            'content_html'   => $item['details'],
-            'url'            => PerchUtil::html($domain).'/events/'.$item['slug'],
-        ];
+        foreach($items as $item) {
+            $feed['items'][] = [
+                'id'             => $item['_id'],
+                'date_published' => date('c', strtotime($item['date'])),
+                'title'          => $item['title'],
+                'content_html'   => $item['details'],
+                'url'            => PerchUtil::html($domain).'/events/'.$item['slug'],
+            ];
+        }
     }
-}
 
-header('Content-Type: application/json');
-echo json_encode($feed);
+    header('Content-Type: application/json');
+    echo json_encode($feed);
