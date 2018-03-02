0b593c6708d8db61d8270696e259159fe0202de3
Martin Underhill <martin@tempertemper.net>
2018-03-02 15:49:34 +0000

    adds event override template

--- web/cms/templates/layouts/header.php
+++ web/cms/templates/layouts/header.php
@@ -18,30 +18,44 @@
             <?php include($_SERVER['DOCUMENT_ROOT'].'/cms/templates/includes/svg/logo.svg'); ?>
         </a>
         <?php
+
             if (perch_layout_var('page', true) == 'home') {
                 $monthDate = date("1-m-Y", strtotime("+1 month"));
                 PerchSystem::set_var('todaysDate', date('U'));
                 PerchSystem::set_var('nextMonth', date("F", strtotime("+1 month")));
                 PerchSystem::set_var('nextEvent', date('d-m-Y', strtotime('thursday', strtotime($monthDate))));
-                $filters = [
-                    [
-                        'filter' => 'date',
-                        'match'  => 'gte',
-                        'value'  => date('Y-m-d')
-                    ],
-                    [
-                        'filter' => 'eventDisplayDate',
-                        'match'  => 'lte',
-                        'value'  => date('Y-m-d H:i:s')
-                    ],
-                ];
-                perch_collection('Events', [
-                    'template'   => 'event_next.html',
-                    'sort'       => 'date',
-                    'sort-order' => 'ASC',
-                    'count'      => 1,
-                    'filter'     => $filters
+
+                $override_details = perch_content_custom('Event override', [
+                    'skip-template' => 'true',
+                    'return-html'   => 'true',
                 ]);
+                $override = $override_details['0']['override'];
+
+                if ($override == 'override') {
+                    perch_content_custom('Event override', [
+                        'template' => 'event_override.html',
+                    ]);
+                } else {
+                    $filters = [
+                        [
+                            'filter' => 'date',
+                            'match'  => 'gte',
+                            'value'  => date('Y-m-d')
+                        ],
+                        [
+                            'filter' => 'eventDisplayDate',
+                            'match'  => 'lte',
+                            'value'  => date('Y-m-d H:i:s')
+                        ],
+                    ];
+                    perch_collection('Events', [
+                        'template'   => 'event_next.html',
+                        'sort'       => 'date',
+                        'sort-order' => 'ASC',
+                        'count'      => 1,
+                        'filter'     => $filters
+                    ]);
+                };
             };
         ?>
     </div>
