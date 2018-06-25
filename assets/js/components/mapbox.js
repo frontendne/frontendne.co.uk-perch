074fa2cbc7c88ca724edd74ae2307f6d31d7cfbd
Colin Oakley <colin@htmlandbacon.com>
2018-06-25 21:40:40 +0100

    updating map style

--- assets/js/components/mapbox.js
+++ assets/js/components/mapbox.js
@@ -1,15 +1,9 @@
 /* global L */
-require('mapbox.js');
+var mapboxgl = require('mapbox-gl');
 
-L.mapbox.accessToken = 'pk.eyJ1Ijoic2FtZGJlY2toYW0iLCJhIjoiSVk5cS1UTSJ9.0lCowgljkS2VZ_8ToBkPUA';
-var map = L.mapbox.map('map', 'samdbeckham.1cmj716e',{
-    zoomControl: true,
-    scrollZoom: false
+
+mapboxgl.accessToken = 'pk.eyJ1Ijoic2FtZGJlY2toYW0iLCJhIjoiSVk5cS1UTSJ9.0lCowgljkS2VZ_8ToBkPUA';
+new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/samdbeckham/cjiuq7cl44pq12sntmsmsobtm'
 });
-
-map.scrollWheelZoom.disable();
-
-if (map.tap) {
-    map.dragging.disable();
-    map.tap.disable();
-}
