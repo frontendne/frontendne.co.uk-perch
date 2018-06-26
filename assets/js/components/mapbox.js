e008d6598ed782ed654ef341a7a9f750f9c313ba
Colin Oakley <colin@htmlandbacon.com>
2018-06-26 19:09:22 +0100

    adding marker and updating to latest mapbox style

--- assets/js/components/mapbox.js
+++ assets/js/components/mapbox.js
@@ -1,9 +1,28 @@
-/* global L */
 var mapboxgl = require('mapbox-gl');
 
 
 mapboxgl.accessToken = 'pk.eyJ1Ijoic2FtZGJlY2toYW0iLCJhIjoiSVk5cS1UTSJ9.0lCowgljkS2VZ_8ToBkPUA';
-new mapboxgl.Map({
+var map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/samdbeckham/cjiuq7cl44pq12sntmsmsobtm'
 });
+
+
+map.on('click', function(e) {
+    var features = map.queryRenderedFeatures(e.point, {
+      layers: ['frontendne-venue'] // replace this with the name of the layer
+    });
+  
+    if (!features.length) {
+      return;
+    }
+  
+    var feature = features[0];
+  
+    new mapboxgl.Popup({ offset: [0, -25] })
+      .setLngLat(feature.geometry.coordinates)
+      .setHTML('<h3>' + feature.properties.name + '</h3><p>' + feature.properties.address + ', ' + feature.properties.postcode  + '</p>')
+      .setLngLat(feature.geometry.coordinates)
+      .addTo(map);
+  });
+  
